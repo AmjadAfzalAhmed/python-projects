91d05489ac6ec10f42cad21dbe3d96f68a6a9e70
AmjadAfzalAhmed <abhutto2006@gmail.com>
2025-03-26 01:13:14 +0500

    17 completed

---
+++ main.py
@@ -0,0 +1,12 @@
+fruits = ['banana','mangoes','apples','oranges']
+
+new_fruits = fruits
+
+new_fruits.append('grapes')
+print(fruits)
+print(new_fruits)
+new_fruits.remove('banana')
+print(fruits)
+print(new_fruits)
+new_fruits.pop()
+print(fruits)
